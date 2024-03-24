fb1cd402ccf334c524216cee25138955eaebd9ab
Andrés Echeverry <anfeipe10@gmail.com>
2024-03-24 11:58:22 -0500

    comment static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-
+/* 
 const nextConfig = {
 	output: 'export',
 	assetPrefix: 'https://aecheverryb10.github.io/bit-a-bit'
-};
+}; */
 
 export default nextConfig;
