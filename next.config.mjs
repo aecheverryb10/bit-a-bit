ded2f2b5e205e401a27075239bd4a88291fe4fbb
Andrés Echeverry <anfeipe10@gmail.com>
2024-03-24 13:25:39 -0500

    follow example

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
- 
 const nextConfig = {
-	output: 'export'
+  output: 'export',
+  basePath: process.env.REPOSITORY_NAME,
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
