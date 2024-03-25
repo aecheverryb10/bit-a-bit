79ddeff0ea41d8a9704aaa302d090f68c8e73dc2
Andrés Echeverry <anfeipe10@gmail.com>
2024-03-24 19:27:40 -0500

    add first chapter

---
+++ src/data/capitulos.js
@@ -0,0 +1,10 @@
+const Capitulos = [
+  {
+    title: 'Introducción',
+    url: 'introduccion',
+  },
+  {
+    title: 'Ascendencias digitales: perspectivas globales y el caso colombiano',
+    url: 'ascendencias-digitales-perspectivas-globales-y-el-caso-colombiano',
+  },
+];
---
+++ src/pages/_app.js
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+import '../styles/globals.css';
+
+function App({ Component, pageProps }) {
+  return (
+    <>
+      {/* <Header/> */}
+
+      <Head>
+        <title>Bit a bit</title>
+        <link rel='icon' href='./favicon.ico' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
+
+export default App;
