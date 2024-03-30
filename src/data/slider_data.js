a868fab050ba721cfcb9a2cbd131f390ae0cece5
Andrés Echeverry <anfeipe10@gmail.com>
2024-03-30 11:48:59 -0500

    textos

--- src/data/slider_data.js
+++ src/data/slider_data.js
@@ -53,3 +53,61 @@ export const slider_data = [
     ),
   },
 ];
+
+const slider_data2 = [
+  {
+    src: './img/figura_20.jpg',
+    alt: 'Figura 20',
+    caption: (
+      <>
+        <p>
+          <strong>Figura 20.</strong> Salón de la Colección de entomología de la Orinoquía, Universidad Nacional.
+        </p>
+        <p>Fuente: Archivo Fotográfico Unimedios</p>
+        <p>(Alvis García/s.f./Archivo de Sede Orinoquía).</p>
+      </>
+    ),
+  },
+  {
+    src: './img/figura_21.jpg',
+    alt: 'Figura 21',
+    caption: (
+      <>
+        <p>
+          <strong>Figura 21.</strong> Ejemplares de la Colección de entomología de la Orinoquía, Universidad Nacional.
+        </p>
+        <p>Fuente: Archivo Fotográfico Unimedios</p>
+        <p>(Alvis García/s.f./Archivo de Sede Orinoquía).</p>
+      </>
+    ),
+  },
+  {
+    src: './img/figura_22.jpg',
+    alt: 'Figura 22',
+    caption: (
+      <>
+        <p>
+          <b>Figura 22.</b> Ejemplares de insectos de la Colección de entomología de la Orinoquía, Universidad Nacional.
+        </p>
+        <p>Fuente: Archivo Fotográfico Unimedios </p>
+        <p>(Alvis García/s.f./Archivo de Sede Orinoquía).</p>
+      </>
+    ),
+  },
+  {
+    src: './img/figura_23.jpg',
+    alt: 'Figura 23',
+    caption: (
+      <>
+        <p>
+          <b>Figura 23.</b> Ejemplares de mariposas de la Colección de entomología de la Orinoquía, Universidad
+          Nacional.
+        </p>
+        <p>Fuente: Archivo Fotográfico Unimedios</p>
+        <p>(Alvis García/s.f./Archivo de Sede Orinoquía).</p>
+      </>
+    ),
+  },
+];
+
+export { slider_data2 };
