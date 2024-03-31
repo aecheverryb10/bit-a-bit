598861565601e056b4abe368db272a77cdd7bfdd
Vanessa1507 <lauritasuarezg@gmail.com>
2024-03-30 19:40:25 -0500

    Update ScrollMultipleImages

--- src/components/ScrollMultipleImage.js
+++ src/components/ScrollMultipleImage.js
@@ -45,62 +45,40 @@ const ScrollMultipleImages = (props) => {
 	}, { scope: containerRef });
 
 	return (
-		<div ref={containerRef} className='flex justify-stretch'>
-			{/* Images */}
-			<div ref={imagesRef} className='w-full '>
-				<div className='relative' style={{ height: `${images.length + 1}00vh` }}>
+		<div ref={containerRef}>
+			<div className='flex'>
+				{/* Images */}
+				<div ref={imagesRef} className='w-full '>
+					<div className='relative' style={{ height: `${images.length + 1}00vh` }}>
+						{
+							images.map((item, index) => (
+								<div className={`${index === 0 ? "opacity-100" : ""} absolute image--container image--container-${index} h-screen w-full top-0`} key={`images-${item.key}`}>
+									{/* eslint-disable-next-line @next/next/no-img-element */}
+									<img
+										className='image h-full w-full object-cover object-center '
+										alt={item.alt}
+										src={item.img}
+									/>
+								</div>
+							))
+						}
+						<div className={`image--container absolute image--container-0 h-screen w-full top-${images.length + 1}`} />
+					</div>
+				</div>
+				{/* Captions */}
+				<div className='h-full absolute right-0'>
 					{
-						images.map((item, index) => (
-							<div className={`${index===0 ? "opacity-100" : ""} opa absolute image--container image--container-${index} h-screen w-fit top-0`} key={`images-${item.key}`}>
-								{/* eslint-disable-next-line @next/next/no-img-element */}
-								<img
-									className='image h-full w-fill object-cover object-center '
-									alt={item.alt}
-									src={item.img}
-								/>
+						images.map(item => (
+							<div className='h-screen flex justify-end items-end' key={`caption-${item.key}`}>
+								<div className='image--caption right-0 bottom-0 text-white'>
+									{item.caption}
+								</div>
 							</div>
 						))
 					}
-					<div className={`image--container absolute image--container-0 h-screen w-full top-${images.length + 1}`} />
+					<div className='h-screen' />
 				</div>
 			</div>
-			{/* Captions */}
-			<div className='h-full'>
-				{
-					images.map(item => (
-						<div className='h-screen flex justify-end items-end' key={`caption-${item.key}`}>
-							<div className='image--caption right-0 bottom-0 text-white'>
-								{item.caption}
-							</div>
-						</div>
-					))
-				}
-				{/* <div className='h-screen'/> */}
-			</div>
-			{/* <div className={"relative"}>
-				{
-					images.map((image, index) => {
-						const {key, caption, alt, img} = image
-						return (
-							<div key={key} className={`absolute flex h-screen justify-between image--container-${index}`}>
-								<div className='relative w-fit mx-auto'>
-									{/* eslint-disable-next-line @next/next/no-img-element
-									<img
-										className='image h-screen w-fill object-cover object-center'
-										alt={alt}
-										src={img}
-									/>
-								</div>
-								<div className='relative w-2/4'>
-									<div className='absolute image--caption right-0 bottom-0 text-white z-10'>
-										{caption}
-									</div>
-								</div>
-							</div>
-						);
-					})
-				}
-			</div> */}
 		</div>
 	);
 };
